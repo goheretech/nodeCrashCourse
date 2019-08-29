8ccb5c0bdcba927532b5e7d809dd01b54dc6630f
goheretech <clemay@gohere.tech>
2019-08-28 22:52:54 -0500

    tutorial finished

--- index.js
+++ index.js
@@ -1,5 +1,58 @@
-const Person = require('./person');
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
 
-const person1 = new Person('John Doe', 30);
+const server = http.createServer((request, response) => {
+    let filePath = path.join(__dirname, 'public', request.url === '/' ? 'index.html' : request.url);
 
-person1.greeting();
+    //Extension of file
+    let extname = path.extname(filePath);
+
+    //Initial content type
+    let contentType = 'text/html';
+
+    //Check ext
+    switch(extname){
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.css':
+            contentType = 'text/css';
+            break;
+        case '.json':
+            contentType = 'application/json';
+            break;
+        case '.png':
+            contentType = 'image/png';
+            break;
+        case '.jpg':
+            contentType = 'image/jpg';
+            break;        
+    }
+
+    //Read File
+    fs.readFile(filePath, (err, content) => {
+        if(err){
+            if(err.code == 'ENOENT'){
+                //Page not found
+                fs.readFile(path.join(__dirname, 'public', '404.html'), (err, content) => {
+                    response.writeHead(200, {'Content-Type': 'text/html' });
+                    response.end(content, 'utf8');
+                })
+            }else{
+                //Some serve error
+                response.writeHead(500);
+                response.end(`Server Error: ${err.code}`);
+            }
+        }else{
+            //Success
+            response.writeHead(200, { 'Content-Type': contentType });
+            response.end(content, 'utf8');
+        }
+    });
+
+});
+
+const PORT = process.env.PORT || 4400;
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
---
+++ ref/path_demo.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+//Basename
+//console.log(path.basename(__filename));
+
+
+//Directory Name
+//console.log(path.dirname(__filename));
+
+
+//File extension
+//console.log(path.extname(__filename));
+
+//Create path object
+//console.log(path.parse(__filename).base);
+
+//Concatenate Paths
+console.log(path.join(__dirname, 'test', 'hello.html'));
+
+
